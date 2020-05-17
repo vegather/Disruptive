db1600e11c1fac7d74a3f288b2c564ab137fe8eb
Vegard Solheim Theriault <vegard@moon.ai>
2020-05-17 10:11:42 +0200

    Initial Commit

---
+++ DisruptiveAPI/DisruptiveAPI.h
@@ -0,0 +1,19 @@
+//
+//  DisruptiveAPI.h
+//  DisruptiveAPI
+//
+//  Created by Vegard Solheim Theriault on 17/05/2020.
+//  Copyright © 2020 Disruptive Technologies Research AS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DisruptiveAPI.
+FOUNDATION_EXPORT double DisruptiveAPIVersionNumber;
+
+//! Project version string for DisruptiveAPI.
+FOUNDATION_EXPORT const unsigned char DisruptiveAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DisruptiveAPI/PublicHeader.h>
+
+
