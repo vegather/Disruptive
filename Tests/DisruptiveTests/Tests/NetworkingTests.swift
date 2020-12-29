ca98055840fea8fe4128e2a1fdf57e4c132954b0
Vegard Solheim Theriault <vegard.theriault@disruptive-technologies.com>
2020-12-29 13:05:14 +0100

    Added tests for network types

--- Tests/DisruptiveTests/Tests/NetworkingTests.swift
+++ Tests/DisruptiveTests/Tests/NetworkingTests.swift
@@ -9,6 +9,27 @@ import XCTest
 @testable import Disruptive
 
 class NetworkingTests: DisruptiveTests {
+    
+    func testPagedKey() {
+        let fromString = PagedKey(stringValue: "5")
+        XCTAssertEqual(fromString?.intValue, nil)
+        XCTAssertEqual(fromString?.stringValue, "5")
+        
+        let fromInt = PagedKey(intValue: 5)
+        XCTAssertEqual(fromInt?.intValue, 5)
+        XCTAssertEqual(fromInt?.stringValue, "5")
+    }
+    
+    func testFormURLEncodedBody() {
+        XCTAssertEqual(formURLEncodedBody(keysAndValues: [:]), "".data(using: .utf8)!)
+        XCTAssertEqual(formURLEncodedBody(keysAndValues: ["foo": "bar"]), "foo=bar".data(using: .utf8)!)
+        
+        let multi = formURLEncodedBody(keysAndValues: ["foo": "bar", "xyz": "abc"])!
+        if multi != "foo=bar&xyz=abc".data(using: .utf8)! && multi != "xyz=abc&foo=bar".data(using: .utf8)! {
+            XCTFail("Unexpected result: \(multi)")
+        }
+    }
+    
     func testServerUnavailable() {
         MockURLProtocol.requestHandler = { request in
             return (nil, nil, URLError(.badServerResponse))
